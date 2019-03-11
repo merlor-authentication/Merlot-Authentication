3e58b792cdaec9817d905b4639047c1fd89848d1
Tilanie <44407626+Tilanie@users.noreply.github.com>
2019-03-11 12:13:00 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,45 @@
-function Authentication()
+var express = require('express');
+var app = express();
+var clientID;
+
+function Authentication(type, data)
+{
+   switch(type) {
+  case 0:
+      clientId = Facial(data);
+    break;
+  case 1:
+      clientId = NFC(data);
+    break;
+  case 2:
+      clientId = OTP(data);
+    break;
+  case 3:
+      clientId = Type(data);
+    break;
+        
+  default:
+    clientId = Default(data);
+}
+}
+
+function Facial(data)
+{
+   
+}
+function NFC(data)
+{
+   
+}
+function OTP(data)
+{
+   
+}
+function Type(data)
+{
+   
+}
+function Default(data)
 {
    
 }
